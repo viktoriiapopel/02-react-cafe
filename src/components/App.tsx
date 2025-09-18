f1b5c6fe5ce1368f8dca5b0fd7050b12b12af923
Viktoriia Popel <xructa2009@gmail.com>
2025-09-18 14:00:55 +0200

    fix

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+// src/components/App.tsx
+
+import Product from "./Product";
+
+export default function App() {
+  return (
+    <>
+      <h1>Best selling</h1>
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
+        price={14.29}
+      />
+    </>
+  );
+}
